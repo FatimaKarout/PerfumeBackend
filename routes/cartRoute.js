87105ccd1c42f70e253c46e79d09a4ac2284d8ef
fatima <fatimakarout1717@gmail.com>
2023-12-15 10:07:34 +0200

    editingCard

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -7,6 +7,8 @@ const {
     addCart,
     updateCartByID,
     deleteCart,
+    updatePerfumesForUser,
+    getCartByUserID,
 } = require('../controllers/cartController');
 
 router.get('/getAll', getAllCarts);
@@ -14,6 +16,9 @@ router.get('/get/:ID', getCartByID);
 router.post('/add', addCart);
 router.put('/update/:ID', updateCartByID);
 router.delete('/delete/:ID', deleteCart);
+router.put('/updatePerfumes/:userid', updatePerfumesForUser);
+router.get('/getByUserId/:useridd', getCartByUserID);
+
 
 
 module.exports = router;
